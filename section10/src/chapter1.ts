1eb9e8b43e8c310879141c291672a320a4c555f8
movingrock <movingrock99@gmail.com>
2024-11-08 16:25:22 +0900

    feat: section10 공부

---
+++ section10/src/chapter1.ts
@@ -0,0 +1,60 @@
+/**
+ * Partial<T>
+ * -> 부분적인, 일부분의
+ * -> 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+// keyof => 특정 객체 타입으로부터 모든 키를 union 타입으로 추출
+// keyof Post => title | tags | content | thumbnailURL
+// [key in keyof T] => key가 각각의 key에 하나씩 맵핑됨.
+// T[key] 특정 property 추출
+type Partial<T> = {
+  [key in keyof T]?: T[key];
+};
+
+const draft: Partial<Post> = {
+  title: "제목 나중에 짓자",
+  content: "초안...",
+};
+
+/**
+ * Required<T>
+ * -> 필수의, 필수적인
+ * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+type Required<T> = {
+  [key in keyof T]-?: T[key];
+};
+
+const withThumbnailPost: Required<Post> = {
+  title: "제목",
+  tags: ["ts"],
+  content: "",
+  thumbnailURL: "http",
+};
+
+/**
+ * Readonly<T>
+ * -> 읽기전용 수정불가
+ * -> 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
+ */
+
+type Readonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const readonlyPost: Readonly<Post> = {
+  title: "보호된 게시글",
+  tags: [],
+  content: "",
+};
+
+readonlyPost.content = "";
